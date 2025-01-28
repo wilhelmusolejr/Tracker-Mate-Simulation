837c350c1c3bbf3e281c5550d2ce1ec337e073f0
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-01-28 14:30:01 +0800

    new executable

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get values from .env
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+TOKEN = "7649581310:AAG9flCCYC_odicG3TqhgQZfpbwmskm7mv4"
+CHAT_ID = "1559668342"
 
 async def send_telegram_notification(message):
     """Send a notification to Telegram asynchronously"""
